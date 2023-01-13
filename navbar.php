b1e8f2f2c8676166bd435b892e4eec0c88a53dfa
Nidhi Gour <nidhiabhi117@gmail.com>
2023-01-13 08:43:19 +0530

    PhpNavbar

---
+++ navbar.php
@@ -0,0 +1,48 @@
+<div class="wrap">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="bg-wrap">
+            <div class="row">
+              <div class="col-md-6 d-flex align-items-center">
+                <p class="mb-0 phone pl-md-2">
+                  <a href="#" class="mr-2"><span class="fa fa-phone mr-1"></span>+91 97185 69879</a>
+                  <a href="#"><span class="fa fa-paper-plane mr-1"></span> <span class="__cf_email__"
+                      data-cfemail="dba2b4aea9beb6bab2b79bbeb6bab2b7f5b8b4b6">[email&#160;protected]</span></a>
+                </p>
+              </div>
+              <div class="col-md-6 d-flex justify-content-md-end">
+                <div class="social-media">
+                  <p class="mb-0 d-flex">
+                    <a href="#" class="d-flex align-items-center justify-content-center">About Us</a>
+                    <a href="#" class="d-flex align-items-center justify-content-center">Contact Us</a>
+                    <a href="#" class="d-flex align-items-center justify-content-center">Testimonials</a>
+                    <a href="#" class="d-flex align-items-center justify-content-center">Blogs</a>
+                    
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">Accounting</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+        aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="fa fa-bars"></span> Menu
+      </button>
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav m-auto">
+          <li class="nav-item active"><a href="index.html" class="nav-link">Start a Business</a></li>
+          <li class="nav-item"><a href="about.html" class="nav-link">Change in Business</a></li>
+          <li class="nav-item"><a href="services.html" class="nav-link">Tax & Compliance</a></li>
+          <li class="nav-item"><a href="cases.html" class="nav-link">Government Registration</a></li>
+          <li class="nav-item"><a href="blog.html" class="nav-link">Services</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
